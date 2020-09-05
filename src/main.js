150544b7c6467c3e0af1c0b13ddcd5ee3deaf912
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-09-06 02:32:55 +0600

    delete unnessary things

--- src/main.js
+++ src/main.js
@@ -18,10 +18,13 @@ if (process.env.VUE_APP_TEST === 'e2e') {
 }
 
 Vue.use(VueFbCustomerChat, {
-  page_id: 100529931400034, //  change 'null' to your Facebook Page ID,
-  theme_color: '#333333', // theme color in HEX
-  locale: 'en_US', // default 'en_US'
-  logged_in_greeting: 'Welcome to Gang',
+  page_id: 100529931400034,
+  theme_color: '#333333',
+  locale: 'en_US',
+  logged_in_greeting:
+    'শৌখিন কাব্য পেজের পক্ষ থেকে আপনাকে জানাচ্ছি আন্তরিক শুভেচ্ছা',
+  logged_out_greeting:
+    'শৌখিন কাব্য পেজের পক্ষ থেকে আপনাকে জানাচ্ছি আন্তরিক শুভেচ্ছা',
 })
 const app = new Vue({
   router,
--- src/router/layouts/main.vue
+++ src/router/layouts/main.vue
@@ -13,6 +13,16 @@ export default {
     <Header />
     <MenuBar />
     <!-- <div class="fb-customerchat" page_id="422328781181196"> FACEBOOK</div> -->
+    <!-- Your Chat Plugin code -->
+    <div
+      class="fb-customerchat"
+      attribution="setup_tool"
+      page_id="100529931400034"
+      theme_color="#0084ff"
+      logged_in_greeting="শৌখিন কাব্য পেজের পক্ষ থেকে আপনাকে জানাচ্ছি আন্তরিক শুভেচ্ছা"
+      logged_out_greeting="শৌখিন কাব্য পেজের পক্ষ থেকে আপনাকে জানাচ্ছি আন্তরিক শুভেচ্ছা"
+    >
+    </div>
     <slot />
     <CheckoutModal />
     <Footer />
