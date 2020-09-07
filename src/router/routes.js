896e15bb4ceb775ea478f3aa439e5d2b2db0fd31
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-09-07 15:06:27 +0600

    block routers

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,94 +1,94 @@
-import store from '@state/store'
-import ProductDetails from '@views/ProductDetails.vue'
+// import store from '@state/store'
+// import ProductDetails from '@views/ProductDetails.vue'
 export default [
   {
     path: '/',
     name: 'home',
     component: () => lazyLoadView(import('@views/home.vue')),
   },
-  {
-    path: '/product',
-    name: 'ProductDetails',
-    component: () => lazyLoadView(import('@views/ProductDetails.vue')),
-  },
-  {
-    path: '/product/:key',
-    name: 'details-product',
-    component: ProductDetails,
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => lazyLoadView(import('@views/login.vue')),
-    meta: {
-      beforeResolve(routeTo, routeFrom, next) {
-        // If the user is already logged in
-        if (store.getters['auth/loggedIn']) {
-          // Redirect to the home page instead
-          next({ name: 'home' })
-        } else {
-          // Continue to the login page
-          next()
-        }
-      },
-    },
-  },
-  {
-    path: '/profile',
-    name: 'profile',
-    component: () => lazyLoadView(import('@views/profile.vue')),
-    meta: {
-      authRequired: true,
-    },
-    props: (route) => ({ user: store.state.auth.currentUser || {} }),
-  },
-  {
-    path: '/profile/:username',
-    name: 'username-profile',
-    component: () => lazyLoadView(import('@views/profile.vue')),
-    meta: {
-      authRequired: true,
-      // HACK: In order to share data between the `beforeResolve` hook
-      // and the `props` function, we must create an object for temporary
-      // data only used during route resolution.
-      tmp: {},
-      beforeResolve(routeTo, routeFrom, next) {
-        store
-          // Try to fetch the user's information by their username
-          .dispatch('users/fetchUser', { username: routeTo.params.username })
-          .then((user) => {
-            // Add the user to `meta.tmp`, so that it can
-            // be provided as a prop.
-            routeTo.meta.tmp.user = user
-            // Continue to the route.
-            next()
-          })
-          .catch(() => {
-            // If a user with the provided username could not be
-            // found, redirect to the 404 page.
-            next({ name: '404', params: { resource: 'User' } })
-          })
-      },
-    },
-    // Set the user from the route params, once it's set in the
-    // beforeResolve route guard.
-    props: (route) => ({ user: route.meta.tmp.user }),
-  },
-  {
-    path: '/logout',
-    name: 'logout',
-    meta: {
-      authRequired: true,
-      beforeResolve(routeTo, routeFrom, next) {
-        store.dispatch('auth/logOut')
-        const authRequiredOnPreviousRoute = routeFrom.matched.some(
-          (route) => route.meta.authRequired
-        )
-        // Navigate back to previous page, or home as a fallback
-        next(authRequiredOnPreviousRoute ? { name: 'home' } : { ...routeFrom })
-      },
-    },
-  },
+  // {
+  //   path: '/product',
+  //   name: 'ProductDetails',
+  //   component: () => lazyLoadView(import('@views/ProductDetails.vue')),
+  // },
+  // {
+  //   path: '/product/:key',
+  //   name: 'details-product',
+  //   component: ProductDetails,
+  // },
+  // {
+  //   path: '/login',
+  //   name: 'login',
+  //   component: () => lazyLoadView(import('@views/login.vue')),
+  //   meta: {
+  //     beforeResolve(routeTo, routeFrom, next) {
+  //       // If the user is already logged in
+  //       if (store.getters['auth/loggedIn']) {
+  //         // Redirect to the home page instead
+  //         next({ name: 'home' })
+  //       } else {
+  //         // Continue to the login page
+  //         next()
+  //       }
+  //     },
+  //   },
+  // },
+  // {
+  //   path: '/profile',
+  //   name: 'profile',
+  //   component: () => lazyLoadView(import('@views/profile.vue')),
+  //   meta: {
+  //     authRequired: true,
+  //   },
+  //   props: (route) => ({ user: store.state.auth.currentUser || {} }),
+  // },
+  // {
+  //   path: '/profile/:username',
+  //   name: 'username-profile',
+  //   component: () => lazyLoadView(import('@views/profile.vue')),
+  //   meta: {
+  //     authRequired: true,
+  //     // HACK: In order to share data between the `beforeResolve` hook
+  //     // and the `props` function, we must create an object for temporary
+  //     // data only used during route resolution.
+  //     tmp: {},
+  //     beforeResolve(routeTo, routeFrom, next) {
+  //       store
+  //         // Try to fetch the user's information by their username
+  //         .dispatch('users/fetchUser', { username: routeTo.params.username })
+  //         .then((user) => {
+  //           // Add the user to `meta.tmp`, so that it can
+  //           // be provided as a prop.
+  //           routeTo.meta.tmp.user = user
+  //           // Continue to the route.
+  //           next()
+  //         })
+  //         .catch(() => {
+  //           // If a user with the provided username could not be
+  //           // found, redirect to the 404 page.
+  //           next({ name: '404', params: { resource: 'User' } })
+  //         })
+  //     },
+  //   },
+  //   // Set the user from the route params, once it's set in the
+  //   // beforeResolve route guard.
+  //   props: (route) => ({ user: route.meta.tmp.user }),
+  // },
+  // {
+  //   path: '/logout',
+  //   name: 'logout',
+  //   meta: {
+  //     authRequired: true,
+  //     beforeResolve(routeTo, routeFrom, next) {
+  //       store.dispatch('auth/logOut')
+  //       const authRequiredOnPreviousRoute = routeFrom.matched.some(
+  //         (route) => route.meta.authRequired
+  //       )
+  //       // Navigate back to previous page, or home as a fallback
+  //       next(authRequiredOnPreviousRoute ? { name: 'home' } : { ...routeFrom })
+  //     },
+  //   },
+  // },
   {
     path: '/404',
     name: '404',
@@ -102,7 +102,7 @@ export default [
   // https://router.vuejs.org/en/essentials/history-mode.html#example-server-configurations
   {
     path: '*',
-    redirect: '404',
+    redirect: '/',
   },
 ]
 
