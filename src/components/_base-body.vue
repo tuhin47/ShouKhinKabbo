d8595e9561bdc48e2224d45d3fdbb4b7cdc7e363
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-09-10 16:39:54 +0600

    demo

---
+++ src/components/_base-body.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: {
+    title: { type: String, default: null },
+    subtitle: { type: String, default: null },
+    loader: { type: Boolean, default: false },
+  },
+}
+</script>
+
+<template>
+  <div />
+</template>
