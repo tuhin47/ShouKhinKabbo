4cd5cdf655e7716f5d24243890a00e739bfd84f9
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-09-12 01:56:45 +0600

    router link

--- src/app.vue
+++ src/app.vue
@@ -14,10 +14,6 @@ export default {
 
 <template>
   <div id="app">
-    <!--
-    Even when routes use the same component, treat them
-    as distinct and create the component again.
-    -->
     <RouterView :key="$route.fullPath" />
   </div>
 </template>
